b4a624de3a521e7ec4ef14b0f7d5128d14a3ca5f
milanvanbruggen <75628000+milanvanbruggen@users.noreply.github.com>
2023-06-07 20:59:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="transformers")
 import streamlit as st
 import torch
 from transformers import RobertaTokenizer, RobertaModel
