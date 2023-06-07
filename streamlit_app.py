5d920c9f7f7c206a4696a6bb772fcbe66c86875b
milanvanbruggen <75628000+milanvanbruggen@users.noreply.github.com>
2023-06-07 20:49:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,90 +1,66 @@
 import streamlit as st
-from sklearn.metrics.pairwise import cosine_similarity
-from transformers import BertTokenizer, BertModel
 import torch
+from transformers import RobertaTokenizer, RobertaModel
 
-# Load pre-trained model tokenizer (vocabulary)
-tokenizer = BertTokenizer.from_pretrained('bert-large-multilingual-uncased')
-# Load pre-trained model (weights)
-model = BertModel.from_pretrained('bert-large-multilingual-uncased')
-
-# Set the model in evaluation mode to deactivate the DropOut modules
-model.eval()
-
-# Maximum sequence length
-max_length = 512
-
-# Function to encode input
-def encode_input(text):
-    tokenized_text = tokenizer.tokenize(text)
-    tokenized_text = tokenized_text[:max_length]  # Truncate or pad to max_length
-    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-    tokens_tensor = torch.tensor([indexed_tokens])
-    return tokens_tensor
-
-# Function to run BERT model
-def run_bert(tokens_tensor):
-    with torch.no_grad():
-        outputs = model(tokens_tensor)
-        sequence_output = outputs[0]  # Use sequence_output instead of pooled_output
-    return sequence_output
-
-# Function to calculate similarity score
-def calculate_similarity(candidate_encoded, company_encoded):
-    score = cosine_similarity(candidate_encoded.detach().numpy(), company_encoded.detach().numpy())
-    return score
+# Model laden
+model_name = 'roberta-base'
+tokenizer = RobertaTokenizer.from_pretrained(model_name)
+model = RobertaModel.from_pretrained(model_name)
 
 # Streamlit app
-st.title('Job Matching App')
+st.title("Job Matching App")
 
-# Organisation input
-st.header('Organisation Information')
-org_description = st.text_area("What kind of company is it?")
-org_values = st.text_area("What are the core values?")
-org_role = st.text_area("What is the open position/role?")
+# Organisatie input
+st.header("Organisatie informatie")
+company_name = st.text_input("Wat is de naam van het bedrijf?")
+core_values = st.text_area("Wat zijn de kernwaarden van het bedrijf?")
+job_role = st.text_input("Wat is de openstaande functie/rol?")
 
-# Candidates input
-st.header('Candidates Information')
-num_candidates = st.number_input('Number of candidates', min_value=1, value=1, step=1)
+# Kandidaat input
+st.header("Kandidaten informatie")
+num_candidates = st.number_input("Aantal kandidaten", min_value=1, value=1, step=1)
 
-candidate_info = []
+candidates = []
 for i in range(num_candidates):
-    with st.expander(f"Candidate {i+1}"):
-        name = st.text_input(f"Candidate {i+1} Name")
-        cv = st.file_uploader(f"Upload CV for candidate {i+1}", type=['txt', 'pdf'])
-        motivation = st.file_uploader(f"Upload Motivation Letter for candidate {i+1}", type=['txt', 'pdf'])
-        candidate_info.append({
-            "name": name,
-            "cv": cv,
-            "motivation": motivation
-        })
+    st.subheader(f"Kandidaat {i+1}")
+    candidate_name = st.text_input("Naam van de kandidaat")
+    cv_file = st.file_uploader("Upload CV (PDF)", type="pdf")
+    motivation_file = st.file_uploader("Upload motivatiebrief (PDF)", type="pdf")
+    if candidate_name and cv_file and motivation_file:
+        candidate = {
+            "name": candidate_name,
+            "cv": cv_file,
+            "motivation": motivation_file
+        }
+        candidates.append(candidate)
 
-# Process candidate information on button click
-if st.button("Calculate Matching"):
-    for candidate in candidate_info:
-        st.subheader(f"Matching for {candidate['name']}")
-        candidate_text = ""
-        if candidate["cv"] is not None:
-            candidate_text += candidate["cv"].read().decode(errors='ignore')
-        candidate_text += " "
-        if candidate["motivation"] is not None:
-            candidate_text += candidate["motivation"].read().decode(errors='ignore')
+# Kandidaten vergelijken
+if st.button("Match kandidaten"):
+    for candidate in candidates:
+        st.subheader(f"Match voor kandidaat: {candidate['name']}")
+        cv_text = candidate["cv"].read().decode()
+        motivation_text = candidate["motivation"].read().decode()
 
-        company_text = org_description + " " + org_values + " " + org_role
+        # Tokenizen van de tekst
+        inputs = tokenizer.encode_plus(
+            cv_text,
+            motivation_text,
+            add_special_tokens=True,
+            max_length=512,
+            truncation=True,
+            padding="max_length",
+            return_tensors="pt"
+        )
+        input_ids = inputs["input_ids"]
+        attention_mask = inputs["attention_mask"]
 
-        candidate_tokens = encode_input(candidate_text)
-        company_tokens = encode_input(company_text)
+        # Model inferentie
+        with torch.no_grad():
+            outputs = model(input_ids, attention_mask)
+            embeddings = outputs.last_hidden_state[:, 0, :]
 
-        candidate_tokens = candidate_tokens.to(torch.long)  # Convert to long tensor
-        company_tokens = company_tokens.to(torch.long)  # Convert to long tensor
+        # Doe iets met de embeddings, bijvoorbeeld bereken de cosine similarity met de organisatie embeddings
+        # ... je code hier ...
 
-        candidate_encoded = run_bert(candidate_tokens)
-        company_encoded = run_bert(company_tokens)
-
-        if candidate_encoded.size(1) == 0:
-            st.warning("Candidate encoding failed. Please check the input.")
-        elif company_encoded.size(1) == 0:
-            st.warning("Company encoding failed. Please check the input.")
-        else:
-            similarity_score = calculate_similarity(candidate_encoded[:, 0], company_encoded[:, 0])
-            st.success(f"Similarity score for {candidate['name']}: {similarity_score}")
+        # Toon de resultaten
+        st.write("Score: ...")  # Vul hier de score in op basis van de embeddings
